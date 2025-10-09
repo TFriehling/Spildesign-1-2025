c49a37eb9974b0ef6d4acf43dbe88d8f26ac2795
ValdeMorgen <valdemarmorgendk@gmail.com>
2025-10-09 13:55:09 +0200

    Added Acceleration to P2
    Added acceleration to NormalPlayerMovement script. Removed fixedUpdate function as Time.deltaTime has been used instead.

--- Spildesign 2025 semester projekt/Assets/NormalPlayerMovement.cs
+++ Spildesign 2025 semester projekt/Assets/NormalPlayerMovement.cs
@@ -6,29 +6,39 @@ public class NormalPlayerMovement : MonoBehaviour
     public float speed;
     Vector2 Dir = new Vector2(0, 0);
     public Rigidbody2D rb;
+    public float maxSpeed;
+    public float acceleration;
+    public float deAcceleration;
 
     void Update()
     {
-        Dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
+        
+        //angle for move dir
+        float angle = Mathf.Atan2(Dir.y, Dir.x) * Mathf.Rad2Deg;
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
-            //angle for move dir
-            float angle = Mathf.Atan2(Dir.y, Dir.x) * Mathf.Rad2Deg;
+            Dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (speed <= maxSpeed && speed >= -1)
+            {
+                speed += (Time.deltaTime * acceleration);
+
+            }
+
+
             //rotate player
-            transform.rotation = Quaternion.Euler(0, 0, angle-90);
+            transform.rotation = Quaternion.Euler(0, 0, angle - 90);
 
         }
+        
+        if (speed >= 0 && Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+        {
+            speed -= Time.deltaTime * deAcceleration;
+        }
 
+        rb.linearVelocity = new Vector2(Dir.x, Dir.y).normalized*speed;
+        Debug.Log(speed);
 
     }
-
-    void FixedUpdate()
-    {
-        rb.linearVelocity = new Vector2(Dir.x, Dir.y).normalized * speed;
-    }
-
-
 
 
 }
